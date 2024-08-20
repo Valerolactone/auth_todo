418a010977f1f9fc83c0cbbbb2b5a2a5a926a1bd
Valerolactone <125586170+Valerolactone@users.noreply.github.com>
2024-08-20 21:44:17 +0300

    Jwt token (#7)

--- main.py
+++ main.py
@@ -1,12 +1,55 @@
+import os
+import re
+
+import httpx
 import uvicorn
 from dotenv import load_dotenv
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, FastAPI, Request
+from fastapi.responses import JSONResponse
 
 from app.routers import login_router, user_router
 
 load_dotenv()
 
 app = FastAPI(title='TODO-Auth')
+
+REDIRECT_MAP = {
+    r'^/core/(.*)$': f'{os.getenv("DRF_URL")}',
+}
+
+
+@app.middleware("http")
+async def regex_redirect_middleware(request: Request, call_next):
+    for pattern, target_url in REDIRECT_MAP.items():
+        match = re.match(pattern, request.url.path)
+        if not match:
+            continue
+
+        auth_header = (
+            {"Authorization": request.headers["Authorization"]}
+            if "Authorization" in request.headers
+            else {}
+        )
+        new_url = f"{target_url}/{match.group(1)}"
+
+        request_data = (
+            request.query_params if request.method in ["GET", "DELETE"] else None
+        )
+        json_data = await request.json() if request.method in ["POST", "PUT"] else None
+
+        async with httpx.AsyncClient() as client:
+            response = await client.request(
+                request.method,
+                new_url,
+                headers=auth_header,
+                json=json_data,
+                params=request_data,
+            )
+
+        return JSONResponse(content=response.json(), status_code=response.status_code)
+
+    response = await call_next(request)
+    return response
 
 
 @app.get("/")

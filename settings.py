6d99ff335bc442a290f6f4e27ee8b3cdd09fd29e
Valerolactone <varanchukv@gmail.com>
2024-09-13 22:32:11 +0300

    9

--- settings.py
+++ settings.py
@@ -27,11 +27,11 @@ class Settings(BaseSettings):
     jwt_algorithm: str = Field(..., env="JWT_ALGORITHM")
     jwt_for_link_secret_key: str = Field(..., env="JWT_FOR_LINK_SECRET_KEY")
 
-    mail_password: Optional[str] = Field(None, env="MAIL_PASSWORD")
-    mail_username: Optional[str] = Field(None, env="MAIL_USERNAME")
-    mail_server: Optional[str] = Field(None, env="MAIL_SERVER")
-    mail_from_name: Optional[str] = Field(None, env="MAIL_FROM_NAME")
-    mail_port: Optional[int] = Field(None, env="MAIL_PORT")
+    mail_password: str = Field(..., env="MAIL_PASSWORD")
+    mail_username: str = Field(..., env="MAIL_USERNAME")
+    mail_server: str = Field(..., env="MAIL_SERVER")
+    mail_from_name: str = Field(..., env="MAIL_FROM_NAME")
+    mail_port: str = Field(..., env="MAIL_PORT")
 
     debug: bool = Field(False, env="DEBUG")
 

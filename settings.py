52e81fe335413e81d1a1e153f6e882022af3ee95
Valerolactone <varanchukv@gmail.com>
2024-09-13 21:39:10 +0300

    updated settings.py

--- settings.py
+++ settings.py
@@ -1,12 +1,13 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Optional
 
 
 class Settings(BaseSettings):
-    app_host: str = Field(..., env="APP_HOST")
-    reset_password_url: str = Field(..., env="RESET_PASSWORD_URL")
-    confirm_registration_url: str = Field(..., env="CONFIRM_REGISTRATION_URL")
-    drf_url: str = Field(..., env="DRF_URL")
+    app_host: Optional[str] = Field(None, env="APP_HOST")
+    reset_password_url: Optional[str] = Field(None, env="RESET_PASSWORD_URL")
+    confirm_registration_url: Optional[str] = not Field(None, env= "CONFIRM_REGISTRATION_URL")
+    drf_url: Optional[str] = Field(None, env="DRF_URL")
 
     db_user: str = Field(..., env="DB_USER")
     db_password: str = Field(..., env="DB_PASSWORD")
@@ -25,11 +26,11 @@ class Settings(BaseSettings):
     jwt_algorithm: str = Field(..., env="JWT_ALGORITHM")
     jwt_for_link_secret_key: str = Field(..., env="JWT_FOR_LINK_SECRET_KEY")
 
-    mail_password: str = Field(..., env="MAIL_PASSWORD")
-    mail_username: str = Field(..., env="MAIL_USERNAME")
-    mail_server: str = Field(..., env="MAIL_SERVER")
-    mail_from_name: str = Field(..., env="MAIL_FROM_NAME")
-    mail_port: int = Field(..., env="MAIL_PORT")
+    mail_password: Optional[str] = Field(None, env="MAIL_PASSWORD")
+    mail_username: Optional[str] = Field(None, env="MAIL_USERNAME")
+    mail_server: Optional[str] = Field(None, env="MAIL_SERVER")
+    mail_from_name: Optional[str] = Field(None, env="MAIL_FROM_NAME")
+    mail_port: Optional[int] = Field(None, env="MAIL_PORT")
 
     debug: bool = Field(False, env="DEBUG")
 
